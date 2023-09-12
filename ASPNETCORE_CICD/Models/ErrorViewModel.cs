434993f89e0aed44a9c766fc28f1e9860e231e06
Nagaraju Bathini <nbathini@ctepl.com>
2023-09-12 19:16:36 +0530

    added

---
+++ ASPNETCORE_CICD/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASPNETCORE_CICD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
